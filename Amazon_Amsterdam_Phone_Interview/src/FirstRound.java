93c29db6e7cf8a41ab7d397b4e9eaf6f7d21bdbe
parimalrupwate <rupwate.parimal5@gmail.com>
2022-03-21 19:04:12 +0530

    Amazon First Telephonic round

---
+++ Amazon_Amsterdam_Phone_Interview/src/FirstRound.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * N= {1,2,5,2,8,9,4,1,5,7,0,4};
+ * K= 3
+ *
+ *t1={1,2,5}= 5
+ *t2={2,5,2}= 5
+ *t3={5,2,8}= 8
+ *t4={2,8,9}= 9
+ *t5={8,9,4}= 9
+ *t6={9,4,1}= 9
+ *t7={4,1,5}= 5
+ *t8={1,5,7}= 7
+ *t9={5,7,0}= 7
+ *t10={7,0,4}=7
+ *
+ *Return HV List
+ *HV List={5,5,8,9,9,9,5,7,7,7}
+ */
+
+public class FirstRound {
+
+	public List<Integer> solution(List<Integer> orders, int k) {
+		List<Integer> hvList=new ArrayList<>();
+		for(int i=0;i<=orders.size()-k;i++) {
+			List<Integer> subOrderList=orders.subList(i, i+k);
+			TreeSet<Integer> set=new TreeSet<>();
+			for(int j=0;j<k;j++) {
+				set.add(subOrderList.get(j));
+			}
+			hvList.add(set.last());
+		}
+		
+		return hvList;
+	}
+	
+	public static void main(String[] args) {
+		FirstRound fr=new FirstRound();
+		List<Integer> orders=new ArrayList<>(Arrays.asList(1,2,5,2,8,9,4,1,5,7,0,4));
+		int k=3;
+		List<Integer> hvList=fr.solution(orders,k);
+		System.out.println(hvList.toString());
+		
+	}
+}
