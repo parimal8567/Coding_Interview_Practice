9c756915eedd412508deeb53fc78edbf66d6a2ea
parimalrupwate <rupwate.parimal5@gmail.com>
2022-04-23 00:18:48 +0530

    amazon phone second interview code added

--- Amazon_Amsterdam_Phone_Interview/src/com/amazon/amsterdam/phone/interview/SecondRound.java
+++ Amazon_Amsterdam_Phone_Interview/src/com/amazon/amsterdam/phone/interview/SecondRound.java
@@ -1,8 +1,53 @@
 package com.amazon.amsterdam.phone.interview;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
- * Will be on 31st March 2022
+ * Find the anagram for the given list
+ * Input ["stop","post","bat","tab","cat"]
  */
 public class SecondRound {
+	
+	public List<List<String>> getAnagram(String[] strs){
+		List<List<String>> output=new ArrayList<>();
+		Map<String,List<String>> layout=new HashMap<>();
+		
+		for(int i=0;i<strs.length;i++) {
+			String str=strs[i];
+			char[] strTemp=str.toCharArray();
+			Arrays.sort(strTemp);
+			String sortedString=String.valueOf(strTemp);
+			
+			char[] charArray=str.toCharArray();
+			Arrays.sort(charArray);
+			String key=String.valueOf(charArray);
+			if(null==layout.get(key)) {
+				List<String> value=new ArrayList<>();
+				value.add(str);
+				layout.put(key, value);
+			}else {
+				List<String> values=layout.get(key);
+				values.add(str);
+			}
+		}
+		
+		for(String str:layout.keySet()) {
+			output.add(layout.get(str));
+		}
+
+		return output;
+	}
+	
+	public static void main(String[] args) {
+		SecondRound sr=new SecondRound();
+		String[] strs= {"eat","tea","tan","ate","nat","bat"};
+		List<List<String>> result=sr.getAnagram(strs);
+		System.out.println("Result:="+result);
+		
+	}
 
 }
